f325132d7dc54fb87d7411e5519e3878b670cd08
ZlatinaMihaylova <zlatina.mihaylova@abv.bg>
2019-01-27 17:16:04 +0200

    Update Demo.java

--- Facebook/src/Demo.java
+++ Facebook/src/Demo.java
@@ -2,7 +2,6 @@
 public class Demo {
 
 	public static void main(String[] args) {
-		User zlati = new User("Zlatina", "zlati@abv.bg", "Zmihaylova97");
 		
 		FacebookLoginSystem login = new FacebookLoginSystem();
 		FacebookLoginSystem.main(null);
